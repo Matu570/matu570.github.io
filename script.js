f125b42ce56cc25bff775c959e5fddb114d0a862
Matias Bueno <51671195+Matu570@users.noreply.github.com>
2024-07-03 23:16:41 -0300

    new functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function saludo() {
+let numeroIngresado;
+function greet() {
     let nombreUsuario=prompt("¿Cuál es su nombre?");
-    alert(nombreUsuario);
+    alert(`Bienvenido ${nombreUsuario}!`);
     return;
 }
 
-saludo();
+function duplicateNumber() {
+    return console.log(4)
+}
+
+function changeText(elementById, text) {
+    return document.getElementById(elementById).innerHTML=text;
+}
+
+changeText("addFunctionDescription","Add a number for duplicate");
+changeText("duplicate","duplicate");
+greet();
+
+
