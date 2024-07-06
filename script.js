c31eacd5f87817d28944506776ea0cabdcf8e536
Matias Bueno <51671195+Matu570@users.noreply.github.com>
2024-07-06 14:43:19 -0300

    Uploading imc calculator

--- script.js
+++ script.js
@@ -2,22 +2,20 @@ let nombreUsuario;
 let result;
 let alturaUsuario;
 let pesoUsuario;
+
 function greet() {
     nombreUsuario=prompt("¿Cuál es su nombre?");
     alert(`Bienvenido ${nombreUsuario}!`);
     return;
 }
-function addData(){
-    return alturaUsuario==undefined?alturaUsuario=parseInt(document.getElementById("imcInput").value):pesoUsuario=parseInt(document.getElementById("imcInput").value);
+
+function changeText(elementById, text) {
+    return document.getElementById(elementById).innerHTML=text;
 }
 
 function duplicateNumber() {
     result=parseInt(document.getElementById(`numberToMultiply`).value)*2;
     return alert(`The result is ${result}`);
-}
-
-function changeText(elementById, text) {
-    return document.getElementById(elementById).innerHTML=text;
 }
 
 function imcStart() {
@@ -26,6 +24,20 @@ function imcStart() {
     changeText("addButtonImc", "Enter")
 }
 
+function addData(){
+    if (alturaUsuario==undefined) {
+        alturaUsuario=parseInt(document.getElementById("imcInput").value)
+        changeText(`imcCalculator`,`${nombreUsuario}, por favor, ingresa tu peso`)
+        document.getElementById("imcInput").value=``;
+    }else {
+        pesoUsuario=parseInt(document.getElementById("imcInput").value);
+        document.getElementById("imcInput").value=``;
+    }
+    if(alturaUsuario!=undefined&&pesoUsuario!=undefined){
+        changeText(`imcCalculator`,`${nombreUsuario}, your IMC is ${pesoUsuario/Math.pow(alturaUsuario,2)}`)
+    }
+    return;
+}
 changeText("addFunctionDescription","Add a number for duplicate");
 changeText("duplicate","duplicate");
 greet();
